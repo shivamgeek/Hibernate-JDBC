f1ea83098aac8efb5529088768e10d4159cdae20
Shivam Aggarwal <shivam01101995@gmail.com>
2020-08-26 14:34:09 +0530

    Checking JDBC connection

--- src/main/java/com/shivam/Hibernate_JDBC/App.java
+++ src/main/java/com/shivam/Hibernate_JDBC/App.java
@@ -1,5 +1,8 @@
 package com.shivam.Hibernate_JDBC;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 /**
  * Hello world!
  *
@@ -8,6 +11,16 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        String jdbcUrl = "jdbc:mysql://localhost:3306/student_database";
+        String user = "root", password = "root";
+        
+        try {
+        	System.out.println("Trying to connect to mysql database");
+        	Connection c = DriverManager.getConnection(jdbcUrl,user,password);
+        	System.out.println("Connection Successful -> "+c);
+        }catch(Exception e) {
+        	e.printStackTrace();
+        }
+        
     }
 }
