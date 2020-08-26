c606d2d5bfec3623dbf456ce492ab5556648f5a6
Shivam Aggarwal <shivam01101995@gmail.com>
2020-08-26 22:07:58 +0530

    Get student object from DB using get

--- src/main/java/com/shivam/Hibernate_JDBC/App.java
+++ src/main/java/com/shivam/Hibernate_JDBC/App.java
@@ -25,7 +25,7 @@ public class App
         
         try {
         	System.out.println("Create a new Student object");
-        	Student s = new Student("Shivam", "Aggarwal","shivam@gmail.com");
+        	Student s = new Student("John", "Wick","John@wick.com");
         	System.out.println("Begin the transaction");
         	session.beginTransaction();
         	System.out.println("Save the Object to DB");
@@ -33,6 +33,20 @@ public class App
         	System.out.println("Commit the changes");
         	session.getTransaction().commit();
         	System.out.println("Done saving to DB");
+        	
+        	System.out.println("Object saved to DB with primary key "+s.getId());
+        	
+        	//Get the data from DB using ID
+        	
+        	session = factory.getCurrentSession();
+        	session.beginTransaction();
+        	
+        	System.out.println("Trying to get Student with ID "+s.getId());
+        	Student tempStud = session.get(Student.class, s.getId());
+        	System.out.println("Info from DB, Student - "+tempStud.getFirstName()+" "+tempStud.getLastName());
+        	session.getTransaction().commit();
+        	System.out.println("Completed");
+        	
         }catch(Exception e) {
         	e.printStackTrace();
         }
