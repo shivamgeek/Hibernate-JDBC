60521ba4ecac5887230c112a043adc497977d166
Shivam Aggarwal <shivam01101995@gmail.com>
2020-08-28 17:40:36 +0530

    Save course in DB and store references in Instructor

--- src/main/java/com/shivam/Hibernate_JDBC/App.java
+++ src/main/java/com/shivam/Hibernate_JDBC/App.java
@@ -6,6 +6,7 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
+import com.shivam.Hibernate_JDBC.Entity.Course;
 import com.shivam.Hibernate_JDBC.Entity.Instructor;
 import com.shivam.Hibernate_JDBC.Entity.InstructorDetail;
 import com.shivam.Hibernate_JDBC.Entity.Student;
@@ -22,13 +23,14 @@ public class App
 
         factory = new Configuration()
         						.configure("hibernate_cfg.xml")
-        						.addAnnotatedClass(Instructor.class).addAnnotatedClass(InstructorDetail.class)
+        						.addAnnotatedClass(Instructor.class).addAnnotatedClass(InstructorDetail.class).addAnnotatedClass(Course.class)
         						.buildSessionFactory();
         //Session session = factory.getCurrentSession();
         
         try {
-        	InstructorDetail det = getInstructorDetailWithId(17);
-        	deleteInstructorDetail(det);
+        	
+        	int id = createAndSaveInstructor();
+        	addCoursesToInstructor(id);
         	
         	
         }catch(Exception e) {
@@ -73,8 +75,8 @@ public class App
     
     static int createAndSaveInstructor() {
     	Session session = factory.getCurrentSession();
-    	Instructor instructor = new Instructor("Iron","man","ironman@hotmail.com");
-    	InstructorDetail idetail = new InstructorDetail("youtube.com/maniron","creating, building stuff");
+    	Instructor instructor = new Instructor("Shivam","Aggarwal","shivam@hotmail.com");
+    	InstructorDetail idetail = new InstructorDetail("youtube.com/shiva","creating, fixing stuff");
     	instructor.setDetail(idetail);
     	session.beginTransaction();
     	session.save(instructor); //this should save idetail object as well due CASCADING.ALL
@@ -83,6 +85,19 @@ public class App
     	return instructor.getId();
     }
 
+    static void addCoursesToInstructor(int inst_id) {
+    	Instructor inst = getInstructorWithId(inst_id);
+    	Session session = factory.getCurrentSession();
+    	session.beginTransaction();
+    	Course c1 = new Course("Engineering");
+    	Course c2 = new Course("Physics");
+    	inst.addCourse(c1);
+    	inst.addCourse(c2);
+    	session.save(c1);
+    	session.save(c2);
+    	session.getTransaction().commit();
+    	System.out.println("Transaction committed");
+    }
     
     
 }
--- src/main/java/com/shivam/Hibernate_JDBC/Entity/Instructor.java
+++ src/main/java/com/shivam/Hibernate_JDBC/Entity/Instructor.java
@@ -6,6 +6,7 @@ import java.util.List;
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -36,7 +37,7 @@ public class Instructor {
 	@JoinColumn(name="instructor_detail_id")
 	private InstructorDetail detail;
 	
-	@OneToMany(mappedBy="instructor", cascade= {CascadeType.DETACH,CascadeType.MERGE,CascadeType.PERSIST,CascadeType.REFRESH})
+	@OneToMany(fetch=FetchType.EAGER, mappedBy="instructor", cascade= {CascadeType.DETACH,CascadeType.MERGE,CascadeType.PERSIST,CascadeType.REFRESH})
 	private List<Course> courses;
 
 	public List<Course> getCourses() {
@@ -104,7 +105,7 @@ public class Instructor {
 		
 	}
 	
-	public void add(Course c) {
+	public void addCourse(Course c) {
 		if(courses == null) {
 			courses = new ArrayList<Course>();
 		}
