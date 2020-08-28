819eedcd11c887cf0f6d487092e2538982755a66
Shivam Aggarwal <shivam01101995@gmail.com>
2020-08-28 17:09:52 +0530

    Add course class and setup many to one association

--- src/main/java/com/shivam/Hibernate_JDBC/Entity/Instructor.java
+++ src/main/java/com/shivam/Hibernate_JDBC/Entity/Instructor.java
@@ -1,5 +1,8 @@
 package com.shivam.Hibernate_JDBC.Entity;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -7,6 +10,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
@@ -31,6 +35,17 @@ public class Instructor {
 	@OneToOne(cascade=CascadeType.ALL)
 	@JoinColumn(name="instructor_detail_id")
 	private InstructorDetail detail;
+	
+	@OneToMany(mappedBy="instructor", cascade= {CascadeType.DETACH,CascadeType.MERGE,CascadeType.PERSIST,CascadeType.REFRESH})
+	private List<Course> courses;
+
+	public List<Course> getCourses() {
+		return courses;
+	}
+
+	public void setCourses(List<Course> courses) {
+		this.courses = courses;
+	}
 
 	public int getId() {
 		return id;
@@ -87,5 +102,13 @@ public class Instructor {
 	
 	public Instructor() {
 		
+	}
+	
+	public void add(Course c) {
+		if(courses == null) {
+			courses = new ArrayList<Course>();
+		}
+		courses.add(c);
+		c.setInstructor(this);
 	}
 }
